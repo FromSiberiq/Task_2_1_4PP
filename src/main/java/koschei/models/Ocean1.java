c72820b96455092e58bd2e262767b0e4724bdc0f
Okami <mr.naydyuk2@gmail.com>
2025-03-17 13:14:30 +0800

    Task 2.1.4 v1.0

---
+++ src/main/java/koschei/models/Ocean1.java
@@ -0,0 +1,21 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Ocean1 {
+
+    @Autowired
+    @Qualifier("island2")
+    private Island2 island;
+    public Ocean1(@Qualifier("island2")Island2 island) {
+        this.island = island;
+    }
+
+    @Override
+    public String toString() {
+        return "на океане остров " + island.toString();
+    }
+}
